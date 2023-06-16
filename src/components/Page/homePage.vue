5228e525c0661a46e0f636f88d03a54cb92c08ed
Guilherme Forssetto <guiforssetto07@gmail.com>
2023-06-15 19:50:09 -0700

    catalog htmlCss started

---
+++ src/components/Page/homePage.vue
@@ -0,0 +1,15 @@
+<template>
+    <product-card>
+</template>
+
+<script>
+import ProductCard from '../ProductCard.vue';
+
+export default{
+  components: { ProductCard },
+    name:'HomePage',
+    component:{
+        ProductCard
+    }
+}
+</script>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <figure class="product-card">
+        <img src="http://dummyimage.com/211x100.png/cc0000/ffffff" alt="test">
+        <figcaption>
+            <p>Product Name</p>
+            <span>$99,99</span>
+        </figcaption>
+    </figure>
+</template>
+
+<script>
+export default {
+    name:'ProductCard'
+}
+</script>
