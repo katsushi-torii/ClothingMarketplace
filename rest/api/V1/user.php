3c7a7b9add073b37fb7fb92a65523a47f4e6067e
riki-nakayashiki <riki.nakayashiki@gmail.com>
2023-06-28 18:56:15 -0700

    populated userdata

---
+++ rest/api/V1/user.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once("../../../inc/Config.inc.php");
+// require_once("../../../inc/Entities/Products.class.php");
+require_once("../../../inc/Entities/User.class.php");
+require_once("../../../inc/Ultilities/PDOAgent.class.php");
+// require_once("../../../inc/Ultilities/ProductConverter.class.php");
+require_once("../../../inc/Ultilities/UserConverter.class.php");
+// require_once("../../../inc/Ultilities/DAO/ProductsDAO.class.php");
+require_once("../../../inc/Ultilities/DAO/UserDAO.class.php");
+
+// ProductDAO::startDb();
+UserDAO::startDb();
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET,POST,HEAD,OPTIONS,PUT,DELETE");
+header('Content-Type: application/json; charset=utf-8');
+
+$userId = $_GET['userId'];
+// $userId = ProductDAO::getProductById($productId)->getUserId();
+
+echo json_encode(
+    UserProductConverter::convertUser(
+        UserDAO::getUserById($userId)
+    )
+);
