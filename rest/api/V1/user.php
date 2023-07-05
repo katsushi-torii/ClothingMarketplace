a3943238e916a26c0e6d8c852c1bf48f957dcdad
riki-nakayashiki <riki.nakayashiki@gmail.com>
2023-07-05 13:11:14 -0700

    Created signin

--- rest/api/V1/user.php
+++ rest/api/V1/user.php
@@ -1,15 +1,11 @@
 <?php
 
 require_once("../../../inc/Config.inc.php");
-// require_once("../../../inc/Entities/Products.class.php");
 require_once("../../../inc/Entities/User.class.php");
 require_once("../../../inc/Ultilities/PDOAgent.class.php");
-// require_once("../../../inc/Ultilities/ProductConverter.class.php");
 require_once("../../../inc/Ultilities/UserConverter.class.php");
-// require_once("../../../inc/Ultilities/DAO/ProductsDAO.class.php");
 require_once("../../../inc/Ultilities/DAO/UserDAO.class.php");
 
-// ProductDAO::startDb();
 UserDAO::startDb();
 
 header("Access-Control-Allow-Origin: *");
@@ -17,7 +13,6 @@ header("Access-Control-Allow-Methods: GET,POST,HEAD,OPTIONS,PUT,DELETE");
 header('Content-Type: application/json; charset=utf-8');
 
 $userId = $_GET['userId'];
-// $userId = ProductDAO::getProductById($productId)->getUserId();
 
 echo json_encode(
     UserProductConverter::convertUser(
